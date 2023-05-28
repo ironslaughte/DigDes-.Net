d0ccdb08ba9c0a98e15a5821b8f255c98397601d
ironslaughte <58146305+ironslaughte@users.noreply.github.com>
2023-05-28 16:36:23 +0300

    Add files via upload

---
+++ Домашнее №3/WebService/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextParser;
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost("getWords")]
+        [ProducesResponseType(200, Type = typeof(Dictionary<string, int>))]
+        [ProducesResponseType(400)]
+        public IActionResult UniqWords([FromBody]string content)
+        {
+            Dictionary<string ,int> dict = UniqWordsCounter.PublicGetAllUniqWordsInText(content);
+            return Ok(dict);
+        }
+    }
+}
